24a409ad85e914704bcfd562a633075125aba7f0
Shubham <sk715152@gmail.com>
2025-11-01 22:20:24 +0530

    UI

--- src/components/NewsCardSkeleton.jsx
+++ src/components/NewsCardSkeleton.jsx
@@ -1,12 +1,12 @@
 const NewsCardSkeleton =({className})=>{
     return (<div className={` ${className} flex flex-col rounded-md overflow-hidden`}>
-        <div className="flex-1 bg-gray-300 animate-pulse"></div>
+        <div className="flex-1 bg-gray-400 animate-pulse"></div>
         <div className="p-4 flex flex-col gap-3">
-            <p className="h-3 lg:h-4 w-20 rounded bg-gray-300 animate-pulse"></p>
-            <p className="h-3 lg:h-3 rounded bg-gray-300 animate-pulse"></p>
-            <p className="h-3 lg:h-3 rounded bg-gray-300 animate-pulse"></p>
-            <p className="h-3 lg:h-3 w-[80%] rounded bg-gray-300 animate-pulse"></p>
-            <p className="h-3 lg:h-4 w-24 rounded bg-gray-300 animate-pulse"></p>
+            <p className="h-3 lg:h-4 w-20 rounded bg-gray-400 animate-pulse"></p>
+            <p className="h-3 lg:h-3 rounded bg-gray-400 animate-pulse"></p>
+            <p className="h-3 lg:h-3 rounded bg-gray-400 animate-pulse"></p>
+            <p className="h-3 lg:h-3 w-[80%] rounded bg-gray-400 animate-pulse"></p>
+            <p className="h-3 lg:h-4 w-24 rounded bg-gray-400 animate-pulse"></p>
         </div>
     </div>)
 };
